f4b5b581c8c3aeafe1dfb217c286279dfe028289
wpanas <wpanas@users.noreply.github.com>
2019-11-10 13:43:18 +0100

    complete part 01

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -15,37 +15,33 @@ public class Part01Flux {
 
 //========================================================================================
 
-	Flux<String> emptyFlux() {
-		return Flux.empty();
-	}
+    Flux<String> emptyFlux() {
+        return Flux.empty();
+    }
 
 //========================================================================================
 
-	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
-	Flux<String> fooBarFluxFromValues() {
-		return Flux.just("foo", "bar");
-	}
+    Flux<String> fooBarFluxFromValues() {
+        return Flux.just("foo", "bar");
+    }
 
 //========================================================================================
 
-	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
-	Flux<String> fooBarFluxFromList() {
-		return Flux.fromIterable(Arrays.asList("foo", "bar"));
-	}
+    Flux<String> fooBarFluxFromList() {
+        return Flux.fromIterable(Arrays.asList("foo", "bar"));
+    }
 
 //========================================================================================
 
-	// TODO Create a Flux that emits an IllegalStateException
-	Flux<String> errorFlux() {
-		return Flux.error(new IllegalStateException());
-	}
+    Flux<String> errorFlux() {
+        return Flux.error(new IllegalStateException());
+    }
 
 //========================================================================================
 
-		// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
-	Flux<Long> counter() {
-		return Flux.interval(Duration.ofMillis(100))
-				.take(10);
-	}
+    Flux<Long> counter() {
+        return Flux.interval(Duration.ofMillis(100))
+                .take(10);
+    }
 
 }
